8657fadb33182b3f3a899b7bb93ad1b0dc8f5ef1
Denis Lutz <denis.lutz@gmail.com>
2011-10-20 14:29:43 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'spree'
+gem 'sqlite3'
+gem 'spree_solr_search'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeProc::Application.config.secret_token = '92047f5190cb5d25e0a60c715ff43fa37bdd4096458f06e078c9488c69926f2bde723a2e1311b2500052482fefabfe5ac656c22feaf86b30d36ba96f1086e626'
